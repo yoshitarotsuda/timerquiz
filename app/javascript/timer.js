5713501fb7a947a439ed64b9c19af31bf709c0a9
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-15 18:00:59 +0900

    ひとまずのタイマー機能実装完了

--- app/javascript/timer.js
+++ app/javascript/timer.js
@@ -1,20 +1,30 @@
 window.addEventListener('load',() => {
-  timer();
+  const timerStart = document.getElementById("timer-start");
+  timerStart.addEventListener('click',timer);
 });
 
-let time = 50;
-
 function timer(){
-  time --;
-  let sec = Math.floor(time % 60); // Math.floor 与えられた値を上回らない最小の整数を返す
-  let min = Math.floor(time / 60);
-  let hour = Math.floor(min / 60);
-  document.getElementById("hour").textContent = String(hour);
-  document.getElementById("min").textContent = String(min);
-  document.getElementById("sec").textContent = String(sec);
-  console.log(sec);
-  const timeoutId = setTimeout(timer,1000);
-  if(sec <= 0){
-    clearTimeout(timeoutId);
+  let time = Number(document.getElementById("set-sec").value) + Number(document.getElementById("set-min").value) * 60 + Number(document.getElementById("set-hour").value) * 3600; // 受け取った値を数値に変換する
+  // console.log(time);
+  count();
+  function count(){
+    console.log(time);
+    time --;
+    let sec = Math.floor(time % 60); // Math.floor 与えられた値を上回らない最小の整数を返す
+    let min = Math.floor(time / 60);
+    let hour = Math.floor(time / 60 / 60);
+    document.getElementById("hour").textContent = String(hour);
+    document.getElementById("min").textContent = String(min);
+    document.getElementById("sec").textContent = String(sec);
+  
+    const timeoutId = setTimeout(count,1000);
+    console.log();
+    if(time <= 0){
+      clearTimeout(timeoutId);
+    }
   }
 }
+
+
+
+
