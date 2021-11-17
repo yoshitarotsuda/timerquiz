88a18ae682e63fd51b92f2b0ae3300cea0c7e61c
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-17 14:13:05 +0900

    編集機能の実装

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -26,6 +26,23 @@ class QuizzesController < ApplicationController
     @quiz = Quiz.find(params[:id])
   end
 
+  def edit # 参考資料としてのメモ書きです editは「編集」であり、編集するモデルを特定し、編集された内容を受け取り、updateアクションに内容を送信します。「更新」ではありません。
+    @quiz = Quiz.find(params[:id])
+    if current_user.id == @quiz.user_id # authenticate_user!のみの制限で良いかと考えたが、よくよく考えると別のログインユーザーが編集できてしまうので必要でした。
+    else # unlessの記述の方が良さそうですが、今後editの実行時に記述が入る可能性を考慮しひとまずifで実装しました。
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @quiz = Quiz.find(params[:id])
+    if @quiz.update(quiz_params)
+      redirect_to quiz_path(@quiz.id)
+    else
+      render 'edit'
+    end
+  end
+
   private
   def quiz_params # ストロングパラメーター設置
     params.require(:quiz).permit(:quiz_name, :quiz_category_id, :quiz_level_id, :quiz_text, :quiz_answer, :quiz_sample).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
-  resources :quizzes, only: [:index, :new, :create, :show]
+  resources :quizzes
 end
