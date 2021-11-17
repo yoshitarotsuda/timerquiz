5e1a2c68785ef150a70288d257d0551e4dbfd86a
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-17 13:12:27 +0900

    一覧ページ作成、リンク作成

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,6 +2,7 @@ class QuizzesController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show] # deviceメソッド、ログアウトユーザーの移動先限定
 
   def index
+    @quizzes = Quiz.all.order("created_at DESC")
   end
 
   def new
