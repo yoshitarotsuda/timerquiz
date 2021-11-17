4bfcacad954916f7154c8b3f71318e6f4aebfe6c
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-17 12:26:36 +0900

    quizへのバリデーション設定

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -5,4 +5,12 @@ class Quiz < ApplicationRecord
   belongs_to :quiz_category
   belongs_to :quiz_level
 
+  with_options presence: true do #with_options オプションの記述をまとめるのに使える、if使用時の記述パターンに注意
+    validates :quiz_name
+    validates :quiz_text
+    validates :quiz_answer
+    validates :quiz_category_id, numericality: { other_than: 0, message: "を選択してください"} 
+    validates :quiz_level_id, numericality: { other_than: 0, message: "を選択してください"} 
+  end
+
 end
