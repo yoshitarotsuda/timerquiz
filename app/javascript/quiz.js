eec0fef774ba711203cd49b7ef6aeed4ff8b61bf
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-19 20:53:32 +0900

    リセットボタンの実装

--- app/javascript/quiz.js
+++ app/javascript/quiz.js
@@ -1,18 +1,36 @@
 window.addEventListener('load',() => {
+  quizAll();
+});
 
+function quizAll(){
+  // クイズ要素全ての取得
+  var defoultHTML = document.getElementById("quiz-main").innerHTML;
+  
   // ✖️ ボタンの取得
   const quizClearButtons = document.getElementsByClassName("quiz-clear");
-
 
   // 自動ボタンの取得
   const autoButton = document.getElementById("quiz-auto");
   autoButton.addEventListener('click',clickAutoButton);
   
+  // 検索ボタンの取得
+  const searchButton = document.getElementById("quiz-search");
+  searchButton.addEventListener('click',clickSearchButtun);
+
+  // 元に戻すボタンの取得
+  const resetSearchButton = document.getElementById("reset-search");
+  resetSearchButton.addEventListener('click',clickResetSearchButton);
+
   // ✖️ボタン全てのイベント設定
   for (var i =0; i < quizClearButtons.length; i++){
     quizClearButtons[i].addEventListener('click',clearQuiz);
   }
 
+  // クイズ検索
+  function clickSearchButtun(){
+    console.log(document.getElementById("quiz-search-category").selectedIndex);
+    console.log(document.getElementById("quiz-search-level"));
+  }
   // クイズ削除
   function clearQuiz(e){
     console.log(e.target);
@@ -21,8 +39,13 @@ window.addEventListener('load',() => {
     console.log(quizClear);
     quizClear.remove();
   }
-  
-  //自動ボタン切り替え
+
+  // 元に戻すボタン起動
+  function clickResetSearchButton(){
+    document.getElementById("quiz-main").innerHTML = defoultHTML;
+    quizAll();
+  }
+  // 自動ボタン切り替え
   function clickAutoButton(){
     if (autoButton.id == "quiz-auto"){
       autoButton.id = "quiz-auto-stop";
@@ -37,27 +60,31 @@ window.addEventListener('load',() => {
     }
   }
 
+  // 自動回答 自動削除
   function autoQuiz(atime,qtime){
-    answerIntervalId = setInterval(topAnswerButtonClick,atime * 1000);
+    setTimeout(topAnswerButtonClick,atime * 1000);
     quizClearIntervalId = setInterval(topQuizClearButtonClick,(atime + qtime) * 1000);
+    
+    function topAnswerButtonClick(){
+      let topAnswerButton = document.getElementsByClassName("click-answer")[0];
+      topAnswerButton.click();
+    }
+    function topQuizClearButtonClick(){
+      if(document.getElementsByClassName("click-answer")[0] == null || document.getElementsByClassName("quiz-clear")[0] == null){
+        stopQuiz();
+      }else{
+        setTimeout(topAnswerButtonClick,atime * 1000);
+        let topQuizClearButton = document.getElementsByClassName("quiz-clear")[0];
+        topQuizClearButton.click();
+      }
+    }
   }
   
   function stopQuiz(){
-    clearInterval(answerIntervalId);
     clearInterval(quizClearIntervalId);
   }
 
-  function topAnswerButtonClick(){
-    let topAnswerButton = document.getElementsByClassName("click-answer")[0];
-    topAnswerButton.click();
-  }
-  function topQuizClearButtonClick(){
-    let topQuizClearButton = document.getElementsByClassName("quiz-clear")[0];
-    topQuizClearButton.click();
-    if(document.getElementsByClassName("quiz-clear")[0] == null){
-      stopQuiz();
-    };
-  }
+
 
   
   
@@ -80,5 +107,4 @@ window.addEventListener('load',() => {
 //       console.log();
 //     };
 //   }
-
-});
+}
