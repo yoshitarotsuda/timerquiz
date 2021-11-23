2015482b0c84d1986de575e2b0c9116bb36e487f
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-23 12:10:39 +0900

    Commentモデルにバリデーションを設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :quiz
+  with_options presence: true do
+    validates :user_id
+    validates :quiz_id
+    validates :comment
+  end
 end
