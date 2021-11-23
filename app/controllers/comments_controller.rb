500b66374df0c728912e3855a93b293ce2b19226
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-23 12:09:24 +0900

    コメント投稿フォーム、コメント表示欄簡易実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:comment).merge(user_id: current_user.id, quiz_id: params[:quiz_id])
+    params.permit(:comment).merge(user_id: current_user.id, quiz_id: params[:quiz_id])
   end
 end
