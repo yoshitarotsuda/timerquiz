fd4be1181f5aff3b741032fe159b723ae3cc9f0d
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-23 11:26:32 +0900

    アクション設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, quiz_id: params[:quiz_id])
+  end
 end
